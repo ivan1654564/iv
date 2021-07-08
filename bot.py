a2830125de84ba98ccf5035e202f4a530b9b5513
ivan1654564 <87083677+ivan1654564@users.noreply.github.com>
2021-07-08 21:59:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+import random
 intents = discord.Intents.all()
 
 bot =commands.Bot(command_prefix ="[ " , intents = intents)
@@ -23,5 +23,19 @@ async def on_member_remove(member):
 async def ping (ctx):
     await ctx.send(f"{round(bot.latency*1000)} ms")
 
+    
+#picture in pc
+@bot.command()
+async def picture (ctx):
+    random_pic = random.choice(jdata["pic"])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+
+#picture online
+@bot.command()
+async def online_pic (ctx):
+    random_pic = random.choice(jdata["online_pic"])
+    await ctx.send(random_pic)
+    
 bot.run("ODYyMzQxOTEyNzgyNTY5NDcy.YOW8fQ.7Q9yEpm3JTcP8hlRgUDg6hIaoWA") 
 
